b3d7bd8b3b25b73110151c8fbda12218565e31d4
Nathan Han <natesd05@gmail.com>
2020-12-19 03:02:15 +0000

    First commit of my bash script

--- .dlstatus.sh
+++ .dlstatus.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 MACHINES="dl01 dl02 dl03 dl04 dl05 dl06 dl07 dl08 dl09 dl10 dl11"
 
-for machine in $MACHINES ; do
-	echo $machine
-	ping $machine > /dev/null
+if [ $# -gt 0 ] ; then
+	arg=$*
+
+
+for arg in $* ; do
+	ping -c 1 $arg > /dev/null
 	if [ $? -eq 0 ] ;
 	then
-		echo ":alive"
+		echo "$arg:alive"
 	else
-		echo ":not alive"
+		echo "$arg:not alive"
 	fi
 done
-exit0
+
+else
+
+for machine in $MACHINES ; do
+	ping -c 1 $machine > /dev/null
+	if [ $? -eq 0 ] ;
+	then
+		echo "$machine:alive"
+	else
+		echo "$machine:not alive"
+	fi
+done
+
+fi
+
